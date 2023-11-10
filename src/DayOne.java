b7cd71b11bbbb251a0e5568d00c67b69ffbd420b
Selma Abbassi <selma.abbassi@lm.se>
2023-11-10 23:37:06 +0100

    lösning dag1 uppgift 1

--- src/DayOne.java
+++ src/DayOne.java
@@ -3,8 +3,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class DayOne {
     public static void main(String[] args) {
@@ -12,22 +11,27 @@ public class DayOne {
 
         try (BufferedReader reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)) {
             String line;
+            Stack<Integer> stack = new Stack<>();
+
+            int sumCalories = 0;
 
             while ((line = reader.readLine()) != null) {
-                List<Integer> calories = new ArrayList<>();
-
                 if (line.equals("")) {
-                    System.out.println("Newline found in the line: " + line);
+                    if (stack.isEmpty()) {
+                        stack.push(sumCalories);
+                    } else if (sumCalories > stack.peek()) {
+                        stack.pop();
+                        stack.push(sumCalories);
+                    }
+                    sumCalories = 0;
                 } else {
-                    calories.add(Integer.valueOf(line));
+                    sumCalories += Integer.parseInt(line);
                 }
             }
+
+            System.out.println("Highest calorie count: " + stack.peek());
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private int sumCalories(List<Integer> calories) {
-        return calories.stream().reduce(0, Integer::sum);
     }
 }

7b16cf30b7ecdefd90af84a9a891b8d8b1c859f4
Selma Abbassi <selma.abbassi@lm.se>
2024-10-27 13:24:43 +0100

    Solved day 6 part 2

--- scripts/2020/day6/day6part2.py
+++ scripts/2020/day6/day6part2.py
@@ -1,26 +1,55 @@
+from typing import List
+
 class Group:
     def __init__(self, group):
-        self.persons = self.initPersons(group)
-        print(self.persons)
+        self.persons = []
+        self.initPersons(group)
+        #self.printGroup()
 
     def initPersons(self, group):
-        for person_answer in group:
-            self.persons.append(Person(person_answer))
+        for person_answers in group:
+            self.persons.append(Person(person_answers))
 
-    def printPersons(self):
-        print(self.persons)
+    def printGroup(self):
+        print("Group:", hash(self))
+        for person in self.persons:
+            person.printAnswers()
+
+    def calculateAnswers(self):
+        all_answers = []
+
+        for person in self.persons:
+            all_answers.append(person.getAnswers())
+        
+        common_answers = set(all_answers[0]).intersection(*all_answers[1:])
+        return len(common_answers)
+    
 
 
 class Person:
     def __init__(self, answers):
-        self.answers = answers
+        self.answers = list(answers)
+
+    def printAnswers(self):
+        print(hash(self), self.answers)
+    
+    def getAnswers(self):
+        return self.answers
 
 if __name__ == "__main__":
-    with open('tst.txt', 'r') as file:
+    with open('day6.txt', 'r') as file:
         content = file.read()
 
-    list = list(map(lambda x: x.replace('\n', ''), content.strip().split('\n\n')))
+    strings = list(content.strip().split('\n\n'))
+    list_of_lists = [s.split('\n') for s in strings]
 
-    groups = []
-    for group in list:
+    groups:List[Group] = []
+    for group in list_of_lists:
         groups.append(Group(group))
+    
+    count = 0
+
+    for group in groups:
+        count += group.calculateAnswers()
+
+    print(f"Sum of counts is: {count}")

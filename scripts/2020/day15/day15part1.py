55100240a8bc8de3cbd05225cfb1b5c3f55d9cd1
Selma Abbassi <selmaabbassi@live.se>
2024-12-02 07:23:31 +0100

    Day 2

--- scripts/2020/day15/day15part1.py
+++ scripts/2020/day15/day15part1.py
@@ -3,22 +3,36 @@ def number_is_spoken(number):
         return True
 
 
-def get_next_number(last_number):
-    if number_is_spoken(last_number):
-        
+def get_previously_spoken_turn(number):
+    max_key = 0
+
+    for key, value in memory_game.items():
+        if value == number & key > max_key:
+            max_key = key
+
+
+def get_next_turn(last_turn, last_spoken_number):
+    next_turn = last_turn + 1
+    if number_is_spoken(last_spoken_number):
+        next_number = last_turn - get_previously_spoken_turn(last_spoken_number)
+        return next_turn, next_number
     else:
-        return 0
+        return next_turn, 0
+
 
 def get_last_turn():
     return max(memory_game.keys())
 
-def get_last_spoken_number():
-    last_turn = max(memory_game.keys())
-    return memory_game.get(last_turn)
+
+def get_last_turn_and_value():
+    last_turn = get_last_turn()
+    return (last_turn, memory_game[last_turn])
+
 
 if __name__ == "__main__":
     memory_game = {1: 0, 2: 3, 3: 6}
 
-    while get_last_turn() <= 2020:
-        last_number = get_last_spoken_number()
-        get_next_number(last_number)
+    while get_last_turn <= 2020:
+        last_turn, last_spoken_number = get_last_turn_and_value()
+        turn, number = get_next_turn(last_turn, last_spoken_number)
+        memory_game.update({turn: number})
